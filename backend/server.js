32c89e101ac7e5bff7298bcdf0c155c392eaa38f
Harsh <1.1harsh2.2@gmail.com>
2026-09-24 13:36:10 +0530

    Updated project

--- backend/server.js
+++ backend/server.js
@@ -9,29 +9,29 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors()); // ताकि फ्रंटएंड बिना एरर के बैकएंड से जुड़ सके
+app.use(cors()); 
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected Successfully!'))
     .catch(err => console.error('Database Connection Error:', err));
 
-// 1. REGISTER ROUTE (नया यूज़र बनाने के लिए)
+// 1. REGISTER ROUTE 
 app.post('/api/register', async (req, res) => {
     try {
         const { username, email, password } = req.body;
 
-        // चेक करें कि यूज़र पहले से तो नहीं है
+        
         let userExists = await User.findOne({ email });
         if (userExists) {
             return res.status(400).json({ message: 'Email already registered' });
         }
 
-        // पासवर्ड को सुरक्षित (Encrypt) करें
+        //(Encrypt)
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // नया यूज़र डेटाबेस में सेव करें
+    
         const newUser = new User({
             username,
             email,
@@ -51,13 +51,13 @@ app.post('/api/login', async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        // यूज़र को ईमेल से ढूंढें
+        
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: 'Invalid Email or Password' });
         }
 
-        // पासवर्ड मैच करें
+        
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ message: 'Invalid Email or Password' });
